90000228a163e67c5482607eeeb4853069b8afd6
Aldrich Choi <Aldrich Choi>
2016-12-10 01:52:29 +0800

    Adds something

---
+++ Woodworks/Models/Rule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Woodworks.Models
+{
+    class Rule
+    {
+        private long Rule_id;
+        private String Rule_name;
+        private Dictionary<Wood, float> Woods;
+
+        public long rule_id
+        {
+            get { return Rule_id; }
+            set { Rule_id = value; }
+        }
+
+        public String rule_name
+        {
+            get { return Rule_name; }
+            set { Rule_name = value; }
+        }
+
+        public Dictionary<Wood, float> woods
+        {
+            get { return Woods; }
+            set { Woods = value; }
+        }
+
+    }
+}
